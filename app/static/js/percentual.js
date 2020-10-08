07c82ff07af514e5c8ffe49f511045dddc9a84d4
Pedro Sousa <ppls2106@gmail.com>
2020-10-08 12:17:01 -0300

    add charts

--- app/static/js/percentual.js
+++ app/static/js/percentual.js
@@ -1,17 +1,4 @@
-var app = new Vue({
-    el: '#app',
-    data: {
-        mensagem: 'Olá Vue'
-    }
-})
-
-var app4 = new Vue({
-  el: '#app-4',
-  data: {
-    todos: [
-      { text: 'Aprender JavaScript' },
-      { text: 'Aprender Vue' },
-      { text: 'Criar algo incrível' }
-    ]
-  }
-})
+let primeiro = document.getElementById('primeiro');
+let chart = new Chart(primeiro, {
+    type: 'line'
+});
